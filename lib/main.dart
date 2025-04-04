812a1feb0d14f53edc688cf9f267cd15bec65a2e
alex.ud <alex.ud@webhive.com.ua>
2025-04-04 18:33:50 +0300

    TASK-30244 add pedometer

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 const methodChannel = MethodChannel('flutter_screentime');
 
@@ -35,6 +37,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    initPlatformState();
+    super.initState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  Future<bool> _checkActivityRecognitionPermission() async {
+    bool granted = await Permission.activityRecognition.isGranted;
+
+    if (!granted) {
+      granted = await Permission.activityRecognition.request() == PermissionStatus.granted;
+    }
+
+    return granted;
+  }
+
+  Future<void> initPlatformState() async {
+    bool granted = await _checkActivityRecognitionPermission();
+    if (!granted) {
+      // tell user, the app will not work
+    }
+
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    (_pedestrianStatusStream.listen(onPedestrianStatusChanged)).onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +111,42 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(
+              'Steps Taken',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              _steps,
+              style: TextStyle(fontSize: 30),
+            ),
+            Divider(
+              height: 100,
+              thickness: 0,
+              color: Colors.white,
+            ),
+            Text(
+              'Pedestrian Status',
+              style: TextStyle(fontSize: 20),
+            ),
+            Icon(
+              _status == 'walking'
+                  ? Icons.directions_walk
+                  : _status == 'stopped'
+                      ? Icons.accessibility_new
+                      : Icons.error,
+              size: 50,
+            ),
+            Center(
+              child: Text(
+                _status,
+                style: _status == 'walking' || _status == 'stopped'
+                    ? TextStyle(fontSize: 20)
+                    : TextStyle(fontSize: 20, color: Colors.red),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: () async {
                 var result = 'approved';
@@ -66,6 +168,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 methodChannel.invokeMethod('unblockApp');
               },
               child: const Text('unblockApp'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  
+                });
+              },
+              child: const Text('set state'),
             ),
           ],
         ),
